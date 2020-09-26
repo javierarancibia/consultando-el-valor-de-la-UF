0a775e98410de1c28275d7b4df5dec828a7e8b5f
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-25 22:16:23 -0300

    DELETE first model and started adding UF values for January and February 2020

--- app/models/valor_uf.rb
+++
@@ -1,2 +0,0 @@
-class ValorUf < ApplicationRecord
-end
--- test/models/valor_uf_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ValorUfTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
